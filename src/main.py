d73c8c227753280a009b5e01e262d1d623603913
EthanDGee <workethangee@gmail.com>
2025-03-21 00:10:47 -0600

    Added tutorial, and cleaned up some confusing points for the player.

--- src/main.py
+++ src/main.py
@@ -8,23 +8,33 @@ def display_word_sequence(word_path):
 	print()
 
 
+def tutorial():
+	print("Welcome to Word Connect!")
+	print(
+		"You will be presented with a pair of words, and you will need to get between them in as few moves as possible.")
+	print("You can only change one letter at a time. You can swap a letter, add a letter, or remove a letter.")
+	print(" If you want to quit, type 'q'. Try your best to tie the computer (you will not beat it)")
 
 
 if __name__ == "__main__":
-	graph = Graph("../data/words_trimmed.txt")
 
-	graph.export_graph("../data/graph.graphml")
+	tutorial()
+	graph = Graph("../data/filtered_words.txt")
+
+	# graph.export_graph("../data/graph.graphml")
 
 	possible_words = list(graph.vertexes.keys())
 
 	# Initiate Game Loop
 	while True:
 		print("finding pair...", end='\r')
+
 		random_words = random.sample(possible_words, 2)
 		path = graph.find_shortest_path(random_words[0], random_words[1])
 		guess_count = 0
+		current_word = ""
 		if path:
-			print(f"Pair Found! - {random_words[0]} -> {random_words[1]} in {len(path) - 2}\n{random_words[0]}")
+			print(f"Pair Found! - {random_words[0]} -> {random_words[1]} in {len(path) - 1}\n{random_words[0]}")
 
 			last_word = random_words[0]
 			current_word = ""
@@ -32,13 +42,13 @@ if __name__ == "__main__":
 			# until we reach the goal
 			while last_word != random_words[1]:
 
-				# prompt the user for a word
+				# prompt the user for a wordPAW
 				while not graph.vertexes.get(last_word).has_neighbor(current_word):
 					current_word = input().lower().strip()
 					if current_word == "q":
 						break
 					if current_word not in graph.vertexes:
-						print("Invalid word")
+						print(f"Invalid word - current word {last_word}")
 						current_word = ""
 					else:
 						guess_count += 1
@@ -47,8 +57,9 @@ if __name__ == "__main__":
 					break
 				last_word = current_word
 
-		print(f"You got the word in {guess_count} guesses!")
+		if current_word != "q":
+			print(f"You got the word in {guess_count} guesses!")
 		# an extra 1 is removed from user due to loop issues (there's no do-while loops in python)
-		print(f"The Computer was about to get there in {len(path) - 3} guesses.")
+		print(f"The Computer got there in {len(path)} guesses.")
 		display_word_sequence(path)
-qqq
+		print("\n\n")
